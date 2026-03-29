34107611b0749a2396c106d82944882e14db2763
root <root@srv1455233.hstgr.cloud>
2026-03-29 07:49:58 +0000

    Switch entire site to purple only - remove all orange

--- src/layouts/components/Logo.tsx
+++ src/layouts/components/Logo.tsx
@@ -1,8 +1,8 @@
 const Logo = () => {
   return (
     <a href="/" className="navbar-brand inline-block text-2xl font-bold tracking-tight">
-      <span style={{ color: "#FF5D01" }}>astro</span>
-      <span style={{ color: "#FF5D01" }}>awesome</span>
+      <span style={{ color: "#882DE7" }}>astro</span>
+      <span style={{ color: "#882DE7" }}>awesome</span>
       <span className="text-text-light opacity-50">.com</span>
     </a>
   );
